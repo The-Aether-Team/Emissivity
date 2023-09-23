357fb68d1796748a265165b587e1387361607f77
bconlon <bconlon.gml@gmail.com>
2023-09-23 14:31:59 -0700

    fix: Emissive armor layer not working on other entities

--- src/main/java/com/aetherteam/emissivity/client/renderer/EmissivityRenderers.java
+++ src/main/java/com/aetherteam/emissivity/client/renderer/EmissivityRenderers.java
@@ -7,11 +7,16 @@ import net.minecraft.client.model.HumanoidArmorModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Emissivity.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EmissivityRenderers {
@@ -23,6 +28,8 @@ public class EmissivityRenderers {
         event.registerLayerDefinition(EmissivityModelLayers.PLAYER_OUTER_ARMOR_EMISSIVE, () -> outer);
         event.registerLayerDefinition(EmissivityModelLayers.PLAYER_SLIM_INNER_ARMOR_EMISSIVE, () -> inner);
         event.registerLayerDefinition(EmissivityModelLayers.PLAYER_SLIM_OUTER_ARMOR_EMISSIVE, () -> outer);
+        event.registerLayerDefinition(EmissivityModelLayers.ENTITY_INNER_ARMOR_EMISSIVE, () -> inner);
+        event.registerLayerDefinition(EmissivityModelLayers.ENTITY_OUTER_ARMOR_EMISSIVE, () -> outer);
     }
 
     @SubscribeEvent
@@ -36,6 +43,17 @@ public class EmissivityRenderers {
                         playerRenderer,
                         new HumanoidArmorModel<>(entityModelSet.bakeLayer(type.equals("slim") ? EmissivityModelLayers.PLAYER_SLIM_INNER_ARMOR_EMISSIVE : EmissivityModelLayers.PLAYER_INNER_ARMOR_EMISSIVE)),
                         new HumanoidArmorModel<>(entityModelSet.bakeLayer(type.equals("slim") ? EmissivityModelLayers.PLAYER_SLIM_OUTER_ARMOR_EMISSIVE : EmissivityModelLayers.PLAYER_OUTER_ARMOR_EMISSIVE)),
+                        Minecraft.getInstance().getModelManager()));
+            }
+        }
+        List<EntityType<? extends LivingEntity>> entities = List.of(EntityType.ZOMBIE, EntityType.ZOMBIE_VILLAGER, EntityType.HUSK, EntityType.SKELETON, EntityType.STRAY, EntityType.PIGLIN, EntityType.ZOMBIFIED_PIGLIN, EntityType.ARMOR_STAND);
+        for (EntityType<? extends LivingEntity> entityType : entities) {
+            LivingEntityRenderer<LivingEntity, HumanoidArmorModel<LivingEntity>> renderer = event.getRenderer(entityType);
+            if (renderer != null) {
+                renderer.addLayer(new EmissiveArmorLayer<>(
+                        renderer,
+                        new HumanoidArmorModel<>(entityModelSet.bakeLayer(EmissivityModelLayers.ENTITY_INNER_ARMOR_EMISSIVE)),
+                        new HumanoidArmorModel<>(entityModelSet.bakeLayer(EmissivityModelLayers.ENTITY_OUTER_ARMOR_EMISSIVE)),
                         Minecraft.getInstance().getModelManager()));
             }
         }
